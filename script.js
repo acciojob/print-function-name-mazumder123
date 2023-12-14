4940fee69e97e64c0c8b1ee7d00c82dd8a611d2a
mazumder123 <112567802+mazumder123@users.noreply.github.com>
2023-12-15 00:02:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function functionName() {
+  alert("functionName");
+}
+
+// Example usage:
+functionName(); // This will display an alert with the text "functionName"
